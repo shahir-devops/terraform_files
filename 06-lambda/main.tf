1621510495852f8215606cb354f90618f1183a57
shahir-syed <shahirsyeed@gmail.com>
2026-03-05 22:58:17 +0530

    rds and lambda

---
+++ 06-lambda/main.tf
@@ -0,0 +1,9 @@
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "my-lambda"
+  role          = "arn:aws:iam::956172363228:role/aws_lambda"
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
---
+++ 06-rds/main.tf
@@ -0,0 +1,21 @@
+resource "aws_db_instance" "mysqldb" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.4.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "Cloud123"
+  parameter_group_name = "default.mysql8.4"
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.sub_grp.name
+}
+
+resource "aws_db_subnet_group" "sub_grp" {
+  name = "main"
+  subnet_ids = ["subnet-05e6b9d5eeba6b0e9",
+  "subnet-0cd44f22f73f37f72"]
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
