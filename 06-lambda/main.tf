c2c4b7d3cf96737db049312de5a438eec83bdd88
shahir <shahirsyeed@gmail.com>
2026-04-19 17:19:43 +0530

    Update main.tf

--- 06-lambda/main.tf
+++ 06-lambda/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "my_lambda" {
   function_name = "my-lambda"
   role          = "arn:aws:iam::956172363228:role/aws_lambda"
-  handler       = "lambda.lambda_handler"
+  handler       = "lambda.lambda_handler" #lambda is file name and lambda_handler is a function name 
   runtime       = "python3.9"
 
   filename         = "lambda.zip"
